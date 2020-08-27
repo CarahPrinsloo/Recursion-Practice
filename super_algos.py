0ba81121b4b6c1f17c0379ce8f823ffd7c773c54
CarahPrinsloo <49373177+CarahPrinsloo@users.noreply.github.com>
2020-08-27 16:21:21 +0200

    Update super_algos.py

--- super_algos.py
+++ super_algos.py
@@ -1,16 +1,67 @@
+def is_integer_list(list_given):
+    """Checks if the list contains only integers, returns true if it does"""
+    
+    for item in list_given:
+        try:
+            item = int(item)
+        except:
+            return False
+    return True
 
 def find_min(element):
-    """TODO: complete for Step 1"""
-    pass
+    """Returns the smallest number in the list"""
+
+    if len(element) == 0 or (not is_integer_list(element)):
+        return -1
+    # base case
+    elif len(element) == 1:
+        return element[0]
+    # recursive
+    else:
+        min_number = find_min(element[1:])
+        if (element[0] < int(min_number)):
+            return element[0]
+        return min_number        
 
 
 def sum_all(element):
     """TODO: complete for Step 2"""
-    pass
+    """Returns the sum of the inetegers in the list"""
+    if len(element) == 0 or (not is_integer_list(element)):
+        return -1
+    elif len(element) == 1:
+        return element[0]
+    else:
+        sum = element[0] + sum_all(element[1:])
+    return sum
+
+
+def power_of_number(base, power):
+    """Calculates the power of a number"""
+    if (power == 1):
+        return base
+    else:
+        return (base * power_of_number(base, power-1))
+
+def swap(char_list, index_a, index_b):
+    temp = char_list[index_a]
+    char_list[index_a] = char_list[index_b]
+    char_list[index_b] = temp
+    return(char_list)
 
 
 def find_possible_strings(character_set, n):
     """TODO: complete for Step 3"""
-    pass
+    total_number_sets = power_of_number(n, len(character_set))
+    size_set = len(character_set)
+    if (size_set == 1):
+        for j in range(0, n):
+            character_set
 
 
+def main():
+    ch_str = "ab"
+    find_possible_strings(ch_str, 3)
+
+if __name__ == "__main__":
+    main()
